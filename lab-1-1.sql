7912ad3a544d33b7845468c3d3b86ac2f66b53dd
saifeiwang <saifeiwang2026.5@u.northwestern.edu>
2026-04-15 20:39:10 +0000

    Complete lab from 1 to 3 tasks

--- lab-1-1.sql
+++ lab-1-1.sql
@@ -13,4 +13,5 @@
 -- | 2020 | Washington Nationals              |
 -- +------+-----------------------------------+
 
-
+SELECT name, year
+FROM teams;
--- lab-2-2.sql
+++ lab-2-2.sql
@@ -9,4 +9,6 @@
 -- | 2020 | Chicago White Sox |
 -- +------+-------------------+
 
-
+SELECT name, year
+FROM teams
+WHERE park = "Guaranteed Rate Field";
